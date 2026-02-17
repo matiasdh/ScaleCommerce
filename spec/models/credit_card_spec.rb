016a7655875da250cd62aef60f33839bb9020c5d
Matías de Horta <matias.dh@gmail.com>
2026-02-16 23:29:11 -0300

    test: add specs for CreditCard.build_for_token and CheckoutOrderAtomicService

--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -20,6 +20,29 @@ RSpec.describe CreditCard, type: :model do
     it { should validate_inclusion_of(:exp_month).in_range(1..12) }
   end
 
+  describe ".build_for_token" do
+    let(:token) { "tok_success" }
+    let(:payment_gateway) { PaymentGateway.new(latency: 0) }
+
+    it "returns an unsaved CreditCard with gateway details" do
+      card = described_class.build_for_token(token, payment_gateway:)
+
+      expect(card).to be_a(CreditCard)
+      expect(card).to be_new_record
+      expect(card.token).to eq(PaymentGateway::SUCCESS_TOKEN)
+      expect(card.last4).to eq("4242")
+      expect(card.exp_month).to eq(12)
+      expect(card.exp_year).to eq(2030)
+      expect(PaymentGateway::CARD_BRANDS).to include(card.brand)
+    end
+
+    it "does not persist to the database" do
+      expect {
+        described_class.build_for_token(token, payment_gateway:)
+      }.not_to change(CreditCard, :count)
+    end
+  end
+
   describe ".create_from_token!" do
     let(:token) { "tok_success" }
     let(:payment_gateway) { PaymentGateway.new(latency: 0) }
